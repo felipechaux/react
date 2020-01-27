4ec5e7dcced3cc428a44954b3bc7163b6fb5469f
Felipe chaux <felchaxl@gmail.com>
2020-01-27 17:06:47 -0500

    change

--- heroku-app/src/App.js
+++ heroku-app/src/App.js
@@ -7,14 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Re
-        </a>
+     
       </header>
     </div>
   );
