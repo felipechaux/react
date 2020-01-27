16cbd38666f8147c3bcb44f7f0d0233f07b75a6f
Felipe chaux <felchaxl@gmail.com>
2020-01-27 17:16:33 -0500

    edit

--- heroku-app/src/App.js
+++ heroku-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-     
+     EDICION!!!
       </header>
     </div>
   );
