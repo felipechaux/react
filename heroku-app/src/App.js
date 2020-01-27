5e6b14b034c47424e850336d6e24709c24eab900
Felipe chaux <felchaxl@gmail.com>
2020-01-27 17:20:15 -0500

    edit appjs

--- heroku-app/src/App.js
+++ heroku-app/src/App.js
@@ -5,10 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-     EDICION!!!
-      </header>
+      
     </div>
   );
 }
